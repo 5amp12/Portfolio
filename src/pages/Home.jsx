f3c2f9cf094193c084d82daf48cfc7d1547f1468
srgp2 <srgp2@kent.ac.uk>
2026-04-22 15:19:14 +0100

    feat: adding an introduction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,16 @@ export default function App() {
           <a href="https://www.linkedin.com/in/samuel-passey-10b91a261/"><FaLinkedin size={19}/></a>
         </div>
       </nav>
+
+      <div className="hero">
+        <div className="hero-tag">available for work</div>
+        <h1>
+          <span className="highlight">Samuel Passey</span><br />
+          <span className="dim sub">— developer & builder</span>
+        </h1>
+        <p className="hero-sub">I am a full stack developer based in Cambridge and a Computer Science undergraduate in my final year at the University of Kent. I am seeking a software development role where I can apply my technical skills and practical experience. </p>
+        <p className="hero-sub">I aim to build reliable systems that are well written, well designed, and easy to maintain.</p>
+      </div>
     </>
   )
 }
