f0cf3eda3952717964b383144c2d390426c2abef
srgp2 <srgp2@kent.ac.uk>
2026-04-22 15:23:31 +0100

    feat: skills and education section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,15 @@ import { CiMail } from 'react-icons/ci'
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 export default function App() {
+    const skills = {
+        languages:      ['Python', 'Java'],
+        frontend:       ['React', 'HTML', 'CSS', 'Tailwind'],
+        backend:        ['Node.js', 'FastAPI', 'PostgreSQL', 'MySQL'],
+        infrastructure: ['Docker', 'Git', 'Linux'],
+        Working_Knowledge: ['C++', 'AWS' ]
+    }
+
+
   return (
     <>
       <nav>
@@ -23,6 +32,41 @@ export default function App() {
         <p className="hero-sub">I am a full stack developer based in Cambridge and a Computer Science undergraduate in my final year at the University of Kent. I am seeking a software development role where I can apply my technical skills and practical experience. </p>
         <p className="hero-sub">I aim to build reliable systems that are well written, well designed, and easy to maintain.</p>
       </div>
+
+      <section id="about">
+        <div className="section-label">Skills & Education</div>
+        <div className="about-grid">
+            {/*Skills section*/}
+            <div className="skills-box">
+                <h3 className="skills-title">skills</h3>
+                {Object.entries(skills).map(([category, items]) => (
+                    <div key={category} className="skill-group">
+                        <span className="skill-category">{category}</span>
+                        <div className="stack">
+                            {items.map(i => <span key={i} className="tag">{i}</span>)}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/*Education*/}
+            <div className="skills-box">
+                <h3 className="skills-title">education</h3>
+                <div className="skill-group">
+                    <span className="skill-category">2022 — 2026</span>
+                    <p className="edu-institution">University of Kent</p>
+                    <p className="edu-detail">BSc (Hons) Computer Science</p>
+                    <p className="edu-detail">Avg: 82%</p>
+                </div>
+                <div className="skill-group">
+                    <span className="skill-category">2020 — 2022</span>
+                    <p className="edu-institution">Long Road Sixth Form College</p>
+                    <p className="edu-detail">Computer Science · Business · Chemistry</p>
+                    <p className="edu-detail">D · B · C</p>
+                </div>
+            </div>
+        </div>
+      </section>
     </>
   )
 }
